d5db487b40372537b173424d389e6fa10ef09be5
Alexandre-Milim <alexandremilim15@gmail.com>
2026-02-27 14:33:07 -0300

    Initial commit

---
+++ lua/plugins/lua.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              runtime = {
+                version = "LuaJIT",
+              },
+              diagnostics = {
+                globals = { "vim" },
+              },
+              workspace = {
+                checkThirdParty = false,
+                library = vim.api.nvim_get_runtime_file("", true),
+              },
+              telemetry = { enable = false },
+            },
+          },
+        },
+      },
+    },
+  },
+}
